3429c903d46caadafb1f288b0b14d5ae7e349e36
Laban Eilers <laban@eilers.com>
2013-07-23 16:57:28 -0400

    Better docs automation

--- publish-docs.ps1
+++ publish-docs.ps1
@@ -1,3 +1,4 @@
+grunt docs
 git stash
 git checkout gh-pages
 
@@ -6,13 +7,16 @@ git checkout gh-pages
 
 copy -Force -Recurse .git\docs-temp\* .
 
+echo "Press any key..."
 cmd /c pause | out-null
 
 git add .
 git commit -m "Updating documentation"
 git push
 
+echo "Press any key..."
 cmd /c pause | out-null
 
 git checkout master
+git stash apply
 
