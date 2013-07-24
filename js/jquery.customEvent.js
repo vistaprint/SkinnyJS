ab0b3db6a20522e5f0f87ffb74f6b7c19e03111e
Laban Eilers <laban@eilers.com>
2013-07-24 14:59:42 -0400

    Inline comments for CustomEvent

--- js/jquery.customEvent.js
+++ js/jquery.customEvent.js
@@ -1,11 +1,72 @@
+// ## jQuery.customEvent
+// A class that implements an observable event. 
+
+// This is useful when jQuery custom events aren't appropriate because there's no DOM element involved. 
+
+// This is implemented as a thin wrapper on jQuery.Callbacks().
+
+// ### Usage
+
+// Here's a simple class *Car* that exposes the event *ondrive*:
+
+//     var Car = function()
+//     {
+//         $.CustomEvent.create(this, "ondrive");
+//     
+//         this.drive = function()
+//         {
+//             // TODO some driving implementation
+//             this.ondrive.fire();
+//         }
+//     }
+
+// A consumer of Car could observe the event:
+
+//     var car = new Car();
+//     car.ondrive.add(function() { alert("my car is driving!"); });
+
+// We can also customize the event object that is passed to observers:
+
+//     var Car = function()
+//     {
+//         $.CustomEvent.create(this, "ondrive");
+//     
+//         this.drive = function(speed)
+//         {
+//             // Data will be passed to handlers via the event object
+//             this.ondrive.fire({ speed: speed }); 
+//         }
+//     }
+
+//     var car = new Car();
+//     car.ondrive.add(function(e) { alert("my car is driving at " + e.speed + "mph!"); });
+
+// The event object passed to handlers of jQuery.CustomEvent is a jQuery.Event, and is
+// also returned from jQuery.CustomEvent.fire(). This is useful if you want to use jQuery.Event's methods, 
+// such as *preventDefault()* and *isDefaultPrevented()* to allow observers to influence the behavior of the
+// object.
+
+//     var Car = function()
+//     {
+//         $.CustomEvent.create(this, "ondrive");
+//     
+//         this.drive = function()
+//         {
+//             var evt = this.ondrive.fire(); 
+//             if (evt.isDefaultPrevented())
+//             {
+//                 // Stop driving!!!
+//             }
+//         }
+//     }
+
+// ### Source
+
 (function ($) 
 {
-    /**
-    * @class 
-    * Utility class to manage multiple callbacks.
-    * @constructor
-    * @param {string} eventType The event type (i.e. "close", open")
-    */
+
+    // Utility class to manage multiple callbacks.
+    // @param {string} eventType The event type (i.e. "close", open")
     $.CustomEvent = function(host, eventType)
     {
         this._host = host;
