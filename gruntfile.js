efc93843e012bc0863fb1ac8f88e317c400345d1
Laban Eilers <laban@eilers.com>
2013-07-26 10:35:11 -0400

    Rationalizing site assets

--- gruntfile.js
+++ gruntfile.js
@@ -1,5 +1,3 @@
-/*jshint node: true */
-/*global module */
 
 var generateHomepage = require("./site/generate-homepage");
 var addDocsLinks = require("./site/add-docs-links");
@@ -45,11 +43,24 @@ module.exports = function(grunt)
             {
                 out: ".git/docs-temp/"
             }
+        },
+        copy: 
+        {
+          docs: 
+          {
+            files: 
+            [
+                { expand: true, flatten: true, src: ["site/images/*"], dest: ".git/docs-temp/images/" },
+                { expand: true, flatten: true, src: ["site/javascripts/*"], dest: ".git/docs-temp/javascripts/" },
+                { expand: true, flatten: true, src: ["site/stylesheets/*"], dest: ".git/docs-temp/stylesheets/" }
+            ]
+          }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-qunit');
+    grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-groc');
 
     // Default task(s).
@@ -61,5 +72,5 @@ module.exports = function(grunt)
     // Documentation task(s).
     grunt.registerTask('gen-homepage', "Generates index.html from README.md", generateHomepage);
     grunt.registerTask('add-docs-links', "Adds links to documentation pages", addDocsLinks);
-    grunt.registerTask('docs', ['gen-homepage', 'groc', 'add-docs-links']);
+    grunt.registerTask('docs', ['gen-homepage', 'groc', 'add-docs-links', 'copy:docs']);
 };
