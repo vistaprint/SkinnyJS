dccf7ee3ac248d7d13f9a15ede3f891a086312d2
Laban Eilers <laban@eilers.com>
2013-07-26 20:30:30 -0400

    Updating documentation

--- assets/behavior.js
+++ assets/behavior.js
@@ -677,6 +677,14 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
                     "slug": "documentation"
                   },
                   "depth": 3
+                }, {
+                  "type": "heading",
+                  "data": {
+                    "level": 3,
+                    "title": "Source",
+                    "slug": "source"
+                  },
+                  "depth": 3
                 }
               ]
             }
