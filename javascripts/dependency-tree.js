c9958b6f4b45773a9e67a3fde1d7133bde3fcbd8
Laban Eilers <laban@eilers.com>
2013-08-09 13:37:02 -0400

    Updating documentation

--- javascripts/dependency-tree.js
+++ javascripts/dependency-tree.js
@@ -1,26 +1,38 @@
 var dependencyTree = 
 {
-	"date-parse": [],
-	"jquery.clientRect": [],
-	"jquery.contentSize": ["jquery.clientRect"],
-	"jquery.css": ["jquery.delimitedString"],
-	"jquery.customEvent": [],
-	"jquery.delimitedString": [],
-	"jquery.disableEvent": [],
-	"jquery.hostIframe": [],
-	"jquery.hoverDelay": [],
-	"jquery.htmlEncode": [],
-	"jquery.imageSize": [],
-	"jquery.isVisibleKeyCode": [],
-	"jquery.menu": [],
-	"jquery.modalDialog": ["jquery.queryString","jquery.postMessage","jquery.customEvent","jquery.clientRect","jquery.hostIframe","jquery.proxyAll"],
-	"jquery.modalDialogContent": ["jquery.queryString", "jquery.contentSize", "jquery.customEvent"],
-	"jquery.msAjax": ["date-parse"],
-	"jquery.ns": [],
-	"jquery.partialLoad": [],
-	"jquery.postMessage": [],
-	"jquery.proxyAll": [],
-	"jquery.queryString": ["jquery.delimitedString"],
-	"jquery.uncomment": [],
-	"jquery.url": ["jquery.queryString"]
+	"date-parse": { deps: [] },
+	"jquery.clientRect": { deps: [] },
+	"jquery.contentSize": { deps: ["jquery.clientRect"] },
+	"jquery.css": { deps: ["jquery.delimitedString"] },
+	"jquery.customEvent": { deps: [] },
+	"jquery.delimitedString": { deps: [] },
+	"jquery.disableEvent": { deps: [] },
+	"jquery.hostIframe": { deps: [] },
+	"jquery.hoverDelay": { deps: [] },
+	"jquery.htmlEncode": { deps: [] },
+	"jquery.imageSize": { deps: [] },
+	"jquery.isVisibleKeyCode": { deps: [] },
+	"jquery.menu": { deps: [] },
+	"jquery.modalDialog": 
+	{ 
+		deps: ["jquery.queryString","jquery.postMessage","jquery.customEvent","jquery.clientRect","jquery.hostIframe","jquery.proxyAll"], 
+		single: true, 
+		notes: "Should be included separately, after other skinny.js modules, on pages that open modal dialogs, " +
+			"but NOT in iframe dialog content window documents. Should not be included on the same document as jquery.modalDialogContent." 
+	},
+	"jquery.modalDialogContent": 
+	{ 
+		deps: ["jquery.queryString","jquery.contentSize","jquery.customEvent"], 
+		single: true, 
+		notes: "Should be included separately, after other skinny.js modules, in iframe dialog content windows. " +
+			"Should NOT be included on the same document as jquery.modalDialog." 
+	},
+	"jquery.msAjax": { deps: ["date-parse"] },
+	"jquery.ns": { deps: [] },
+	"jquery.partialLoad": { deps: [] },
+	"jquery.postMessage": { deps: [] },
+	"jquery.proxyAll": { deps: [] },
+	"jquery.queryString": { deps: ["jquery.delimitedString"] },
+	"jquery.uncomment": { deps: [] },
+	"jquery.url": { deps: ["jquery.queryString"] }
 };
--- javascripts/download-builder.js
+++ javascripts/download-builder.js
@@ -1,15 +1,16 @@
-
+/* globals _, dependencyTree */
 
 (function($, tree)
 {
 
+// Build a reverse lookup tree to find dependent modules
 var treeRev = (function(tree)
 {
 	var rev = {};
 
 	for (var prop in tree)
 	{
-		var deps = tree[prop];
+		var deps = tree[prop].deps;
 		for (var i=0; i<deps.length; i++)
 		{
 			if (!rev[deps[i]])
@@ -32,11 +33,11 @@ var buildDeps = function(moduleName, deps)
 	}
 
 	var moduleDeps = tree[moduleName];
-	if (moduleDeps && moduleDeps.length > 0)
+	if (moduleDeps && moduleDeps.deps.length > 0)
 	{
-		for (var i=0; i<moduleDeps.length; i++)
+		for (var i=0; i<moduleDeps.deps.length; i++)
 		{
-			buildDeps(moduleDeps[i], deps);
+			buildDeps(moduleDeps.deps[i], deps);
 		}
 	}
 
@@ -99,6 +100,19 @@ var escapeModuleName = function(unescaped)
 	return unescaped.replace(/\./, "_");
 };
 
+var getLicense = function(modules, requiredModules)
+{
+	return "/*! skinny.js v0.0.1 | Copyright 2013 Laban Eilers | labaneilers.github.io/SkinnyJS/LICENSE \n" +
+		"http://labaneilers.github.io/SkinnyJS/download-builder.html?modules=" + modules.join(",") + 
+		"*/\n\n";
+};
+
+var outputTemplate = _.template($("#outputTemplate").html());
+var buildModuleOutput = function(module)
+{
+	return outputTemplate(module);
+};
+
 var generate = function()
 {
 	var requestedModules = $("form[name=dependencies] input:checked").map(
@@ -107,30 +121,82 @@ var generate = function()
 			return unescapeModuleName(el.value);
 		});
 
+	if (requestedModules.length === 0)
+	{
+		window.alert("Please select at least one module.");
+		return;
+	}
+
+	var $outputArea = $("#outputArea");
+	$outputArea.empty();
+
 	var moduleNames = buildDepsMultiple(requestedModules);
 
-	var content = {};
+	var allContent = {};
 
-	var promises = _.map(moduleNames, function(moduleName) {
-		return $.get("dist-pub/" + moduleName + ".min.js").promise().then(function(responseText) 
-		{
-			content[moduleName] = responseText;
-		});
+	var promises = _.map(moduleNames, function(moduleName) 
+	{
+		return $.ajax({ url: "dist-pub/" + moduleName + ".min.js", dataType: "text"})
+			.then(function(responseText) 
+			{
+				var module = tree[moduleName];
+				allContent[moduleName] = { 
+					name: moduleName,
+					content: responseText,
+					single: module.single, 
+					notes: module.notes
+				};
+			});
 	});
-
-	var license = "/*! skinny.js v0.0.1 | Copyright 2013 Laban Eilers | labaneilers.github.io/SkinnyJS/LICENSE \n" +
-		"http://labaneilers.github.io/SkinnyJS/download-builder.html?modules=" + moduleNames.join(",") + 
-		"*/\n\n";
 
 	$.when.apply(null, promises).then(function()
 	{
-		var temp = promises;
-		var finalContent = license + _.map(moduleNames, function(moduleName)
+		// Get content objects for all modules
+		var allModuleContent = _.map(moduleNames, function(moduleName)
 		{
-			return content[moduleName];
-		}).join(";");
+			return allContent[moduleName];
+		});
 
-		$("#output").html(finalContent);
+		var concatableContent = _.filter(allModuleContent, function(moduleContent)
+		{
+			return !moduleContent.single;
+		});
+
+		var concatableContentNames = _.map(concatableContent, function(moduleContent)
+		{
+			return moduleContent.name;
+		});
+
+		// Filter down to just the non-single modules, then concatenate them
+		// to create a final downloaded file
+		var finalContent = 
+			getLicense(concatableContentNames) + 
+			_.map(concatableContent, function(moduleContent)
+			{
+				return moduleContent.content;
+			})
+			.join(";");
+
+		var html = buildModuleOutput({ name: "skinny.js main module", notes: "", content: finalContent });
+
+		var $form = $("form[name='dependencies']"); 
+
+		// Emit text for single content modules: files that should not be aggregated
+		var singleContent = _.chain(allModuleContent)
+			.filter(function(moduleContent)
+			{
+				return moduleContent.single;
+			})
+			.each(function(moduleContent)
+			{
+				var content = getLicense([moduleContent.name]) + moduleContent.content;
+				html +=
+					"<div class='module-title'>" + moduleContent.name + "</div>" + 
+					"<div class='module-notes'>" + (moduleContent.notes || "") + "<div>" +
+					"<textarea class='module-output'>" + content + "</textarea>";
+			});
+
+		$outputArea.html(html);
 	});
 };
 
