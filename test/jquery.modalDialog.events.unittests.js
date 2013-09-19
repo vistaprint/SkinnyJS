be04a408350c63fd2d3a83c5a77865f36dec9bef
Laban Eilers <laban@eilers.com>
2013-09-19 16:33:46 -0400

    Starting promises api for dialog.open() and dialog.close()

---
+++ test/jquery.modalDialog.events.unittests.js
@@ -0,0 +1,78 @@
+﻿
+$(document).ready(function()
+{
+    function cleanup()
+    {
+        var dialog = $.modalDialog.getCurrent();
+        if (dialog)
+        {
+            dialog.close();
+            stop();
+        }
+    }
+
+    module(
+        "jquery.modalDialog.events",
+        {
+            setup: cleanup,
+            teardown: cleanup
+        });
+
+    // test("Ensure content node can be shared between two dialogs", function()
+    // {
+    //     var dialog = $.modalDialog.create({ content: "#simpleDialog" });
+
+    //     dialog.open();
+
+    //     var dialog2 = $.modalDialog.create({ content: "#simpleDialog" });
+
+    //     dialog2.open();
+
+    //     expect(0);
+    // });
+
+    asyncTest("Ensure node dialog open resolves promise", 1, function()
+    {
+        var dialog = $.modalDialog.create({ content: "#simpleDialog" });
+
+
+        dialog.open()
+            .done(function()
+            {
+                var dialogRef = $.modalDialog.getCurrent();
+                equal(dialogRef, dialog, "Current dialog refs match");
+
+                start();
+            })
+            .fail(function()
+            {
+                ok(false, "promise was rejcted for dialog.open() method");
+                start();
+            });
+    });
+
+    // asyncTest("Ensure node dialog open fires onopen", 1, function()
+    // {
+    //     var dialog = $.modalDialog.create({ content: "#simpleDialog" });
+
+    //     dialog.onopen.add(function()
+    //     {
+    //         equal(this, dialog, "Current dialog refs match");
+
+    //         setTimeout(start, 0);
+    //     });
+
+    //     dialog.open();
+    // });
+
+    // test("Ensure node dialog throws exception when passed non-existent element", function()
+    // {
+    //     throws(function()
+    //     {
+    //         $.modalDialog.create({ content: "#iDontExist" });
+    //     },
+    //     "ModalDialog content not found");
+        
+    // });
+
+});
