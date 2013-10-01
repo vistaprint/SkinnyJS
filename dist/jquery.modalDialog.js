986672cb009d4a4a34b64c971fabe5840ecbeb3c
Laban Eilers <laban@eilers.com>
2013-10-01 15:37:43 -0400

    Updating documentation

--- dist/jquery.modalDialog.js
+++ dist/jquery.modalDialog.js
@@ -271,6 +271,11 @@ if (!Object.keys)
         return !deferred || deferred.state() != "pending";
     };
 
+    ModalDialog.prototype.isOpen = function()
+    {
+        return !!this._open;
+    };
+
     // Opens the dialog
     ModalDialog.prototype.open = function()
     {
@@ -489,16 +494,25 @@ if (!Object.keys)
             delete _fullIdMap[this.settings._fullId];
         }
 
-        this.onclose.fire(e);
-
-        $.modalDialog.onclose.fire(e, this);
-
-        this._resolveDeferred("close");
-
         if ($.modalDialog.isSmallScreen() && this.triggerWindowResize)
         {
             $(window).trigger("resize");
         }
+
+        // Fire events on a timeout so that the event loop
+        // has a chance to process DOM changes. 
+        // Without this, close handlers can't re-open the same iframe dialog:
+        // the iframe isn't recognized as a new element.
+        setTimeout(
+            $.proxy(function()
+            {
+                this.onclose.fire(e);
+
+                $.modalDialog.onclose.fire(e, this);
+
+                this._resolveDeferred("close");
+            }, this), 
+            0);
     };
 
     ModalDialog.prototype._destroy = function()
@@ -539,8 +553,8 @@ if (!Object.keys)
 
             this.$el = $([this.$bg[0], this.$container[0]]).addClass('dialog-skin-' + this.settings.skin);
 
-            // Figure out where to put the dialog DOM. In jQuery mobile, the root element needs to be specific.
-            // It's not for us to fix developer problems, if the container doesn't exist, this will break
+            // HACK: Support jQuery mobile. In jQuery mobile, the root element needs to be specific.
+            // TODO: Move this to a jQuery mobile fixes specific module
             this.parent.append(this.$bg, this.$container);
 
             if (!this.parent.is("body") && !this.parent.hasClass("ui-page-active")) {
@@ -880,7 +894,15 @@ if (!Object.keys)
 
         this._iframeLoadTimer = null;
 
-        this.$frame = $('<iframe src="' + this.settings.url + '" name="' + this.settings._fullId + '" seamless allowtransparency="true" width="100%" style="height:' + this.settings.initialHeight + 'px;" class="dialog-frame" scrolling="no" frameborder="0" framespacing="0" />');
+        if (this.$frame && this.$frame.length > 0)
+        {
+            this.$frame.remove();
+        }
+
+        this.$frame = $('<iframe src="' + this.settings.url + 
+            '" name="' + this.settings._fullId + 
+            '" seamless allowtransparency="true" width="100%" style="height:' + 
+            this.settings.initialHeight + 'px;" class="dialog-frame" scrolling="no" frameborder="0" framespacing="0"></iframe>');
 
         // When the iframe loads, even if its a failed status (i.e. 404), the load event will fire.
         // We expect that the dialog will call notifyReady(). If it doesn't, this timeout will
@@ -923,6 +945,7 @@ if (!Object.keys)
     {
         this._buildContent();
 
+        // TODO Need to somehow notify the dialog content that it should fire notifyReady
         this.$contentContainer.append(this.$content);
     };
 
@@ -1228,11 +1251,15 @@ if (!Object.keys)
 
         var dialog = getDialog(settings._fullId);
 
+        // Validate that there isn't an existing dialog open using the same content
         if (!dialog && settings.content)
         {
-            dialog = $(settings.content).modalDialogInstance();
+            var existingDialog = $(settings.content).modalDialogInstance();
 
-            if (dialog && settings._fullId && dialog.settings._fullId !== settings._fullId && dialog._open)
+            if (existingDialog && 
+                settings._fullId && 
+                existingDialog.settings._fullId !== settings._fullId && 
+                existingDialog.isOpen())
             {
                 throw new Error("An attempt was made to create a dialog with a content node which is already assigned to another open dialog.");
             }
@@ -1284,6 +1311,50 @@ if (!Object.keys)
     $.modalDialog.getCurrent = function()
     {
         return _dialogStack.length > 0 ? _dialogStack[_dialogStack.length-1] : null;
+    };
+
+    // Gets an existing dialog if it's settings match the specified setting's content node or URL
+    $.modalDialog.getExisting = function(settings)
+    {
+        // Supresses warnings about using !! to coerce a falsy value to boolean
+        /* jshint -W018 */
+
+        var $content = $(settings.content);
+        var isMatch;
+
+        // Match a node dialog
+        if ($content && $content.length)
+        {
+            isMatch = function(existingSettings)
+            {
+                return existingSettings.content && 
+                    $(existingSettings.content)[0] === $content[0];
+            };
+        }
+        // match an iframe or ajax dialog
+        else if (settings.url)
+        {
+            isMatch = function(existingSettings)
+            {
+                return existingSettings.url && 
+                    existingSettings.url === settings.url && 
+                    !!existingSettings.ajax === !!settings.ajax;
+            };
+        }
+
+        if (isMatch)
+        {
+            for (var key in _fullIdMap)
+            {
+                var dialog = _fullIdMap[key];
+                if (isMatch(dialog.settings))
+                {
+                    return dialog;
+                }
+            }
+        }
+
+        return null;
     };
 
     // Global events (not associated with an instance)
@@ -1710,3 +1781,244 @@ TODO Make the dialog veil hide earlier when closing dialogs. It takes too long.
 })(jQuery);
 
 
+
+/* globals History */
+// TODO: Need to support stacking dialogs:
+// i.e. ?dialogs=#foo,ajax:/foo.html,iframe:/foo.html
+// TODO require history.js
+
+(function($) 
+{
+    var DEFAULT_DIALOG_PARAM_NAME = "sdialogid";
+    var _dialogParamName;
+
+    // Enables the history plugin, and returns a promise which
+    // resolves when either the dialog specified in the URL is opened,
+    // or if there is no dialog specified, immediately
+    $.modalDialog.enableHistory = function(dialogParamName)
+    {
+        _dialogParamName = dialogParamName || DEFAULT_DIALOG_PARAM_NAME;
+
+        var deferred = new $.Deferred();
+
+        updateFromUrl()
+            .then(function()
+            {
+                $.modalDialog.onopen.add(openHandler);
+                $.modalDialog.onclose.add(closeHandler);
+
+                History.pushState(null, null, document.location.href);
+
+                History.Adapter.bind(window, "statechange", popstateHandler);
+
+                deferred.resolve();
+            });
+
+        return deferred;
+    };
+
+    var _pageIsAtInitialState = true;
+    var _stateAlreadyProcessed = false;
+    var _disableHandlers = false;
+
+    var parseDialogParams = function(data)
+    {
+        if (!data)
+        {
+            return null;
+        }
+
+        var delimPos = data.indexOf(",");
+
+        if (delimPos < 0)
+        {
+            return null;
+        }
+
+        return {
+            dialogType: data.substr(0, delimPos),
+            dialogId: data.substr(delimPos + 1)
+        };
+    };
+
+    var encodeDialogParams = function(dialogType, dialogId)
+    {
+        return dialogType + "," + dialogId;
+    };
+
+    // Handler for dialogs opening
+    var openHandler = function()
+    {
+        // Hook to ensure the history handler doesn't run infinitely
+        // when the dialog was opened by the history plugin itself
+        if (_disableHandlers)
+        {
+            return;
+        }
+
+        // Build a querystring to encode the open state of the dialog
+
+        var dialogType = "node";
+
+        // Get the ID of the selected element (for node dialogs)
+        var dialogId = this.settings.content ? "#" + $(this.settings.content).prop("id") : null;
+
+        // If its not a node dialog, use the URL as the ID
+        if (!dialogId && this.settings.url)
+        {
+            dialogType = this.settings.ajax ? "ajax" : "iframe";
+            dialogId = this.settings.url;
+        }
+
+        var qs = $.currentQueryString();
+        qs[_dialogParamName] = encodeDialogParams(dialogType, dialogId);
+
+        var url = $.appendQueryString(document.location.pathname, qs);
+
+        // Hook to notify the popstate handler that this URL change was triggered internally,
+        // and the dialog is already open, so it shouldn't do any more work.
+        _stateAlreadyProcessed = true;
+
+        // Update the URL
+        History.pushState(null, null, url);
+
+        // Mark the page as not in its initial state so the close handler will know if
+        // it should add a history entry when closing dialogs
+        _pageIsAtInitialState = false;
+    };
+
+    // Handler which fires when dialogs are closed
+    var closeHandler = function()
+    {
+        if (_disableHandlers)
+        {
+            return;
+        }
+
+        // If the page is in its initial state (just loaded), then closing a dialog should
+        // create a new history entry so the back button will open the dialog again.
+        if (_pageIsAtInitialState)
+        {
+            var qs = $.currentQueryString();
+            delete qs[_dialogParamName];
+
+            var url = $.appendQueryString(document.location.pathname, qs);
+
+            History.pushState(null, null, url);
+        }
+        else
+        {
+            // If the page isn't in its initial state, then closing a dialog should go back
+            // one entry in history.
+            History.back();
+        }
+
+        _pageIsAtInitialState = false;
+    };
+
+    // Looks for changes in the URL and opens or closes dialogs accordingly
+    var popstateHandler = function()
+    {
+        // If the history plugin triggered the URL change itself,
+        // then the UI has been updated already, and we shouldn't update anything.
+        if (_stateAlreadyProcessed)
+        {
+            _stateAlreadyProcessed = false;
+            return;
+        }
+
+        updateFromUrl();
+    };
+
+    var updateFromUrl = function()
+    {
+        var deferred = new $.Deferred();
+        var settings;
+        var qs = $.currentQueryString();
+        var dialogParams = parseDialogParams(qs[_dialogParamName]);
+
+        // If the URL is for a dialog, prepare to open the dialog
+        // Build a dialog settings object
+        if (dialogParams)
+        {
+            if (dialogParams.dialogType == "iframe")
+            {
+                settings = { url: dialogParams.dialogId };
+            }
+            else if (dialogParams.dialogType == "ajax")
+            {
+                settings = 
+                { 
+                    ajax: true,
+                    url: dialogParams.dialogId
+                };
+            }
+            else
+            {
+                var $content;
+                try
+                {
+                    $content = $(dialogParams.dialogId);
+                }
+                catch(ex)
+                {}
+
+                if ($content.length > 0)
+                {
+                    settings = $.modalDialog.getSettings($content);
+                    settings.content = $content;
+                }
+            }
+        }
+
+        if (settings)
+        {
+            // Try to reuse an existing, matching dialog if possible
+            var dialog = $.modalDialog.getExisting(settings);
+
+            // If it doesn't exist, create a new one
+            if (!dialog)
+            {
+                dialog = $.modalDialog.create(settings);
+            }
+
+            _disableHandlers = true;
+
+            dialog.open()
+                .then(function()
+                {
+                    setTimeout(function()
+                    {
+                        deferred.resolve();
+                        _disableHandlers = false;
+                    },
+                    0);
+                });
+        }
+        else
+        {
+            var currentDialog = $.modalDialog.getCurrent();
+            if (currentDialog)
+            {
+                _disableHandlers = true;
+                currentDialog.close()
+                    .then(function()
+                    {
+                        setTimeout(function() 
+                        {
+                            deferred.resolve();
+                            _disableHandlers = false;
+                        }, 
+                        0); 
+                    });
+            }
+            else
+            {
+                deferred.resolve();
+            }
+        }
+
+        return deferred;
+    };
+
+})(jQuery);
