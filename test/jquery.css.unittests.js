656652fccc24cc2918ddb3a7381ec17fcecda5b0
Laban Eilers <laban@eilers.com>
2013-10-04 19:32:02 -0400

    More mochafication

---
+++ test/jquery.css.unittests.js
@@ -0,0 +1,76 @@
+﻿describe("jquery.css", function()
+{
+    it("$.camelToDashCase", function()
+    {
+        //basic conversion
+        var actual = $.camelToDashCase("fontFamily");
+        expect(actual).to.be("font-family");
+
+        //Verify we don't break an already converted string
+        actual = $.camelToDashCase("font-family");
+        expect(actual).to.be("font-family");
+    });
+
+    it("$.dashToCamelCase", function()
+    {
+        //basic conversion
+        var actual = $.dashToCamelCase("font-family");
+        expect(actual).to.be("fontFamily");
+
+        //Verify we don't break an already converted string
+        actual = $.dashToCamelCase("fontFamily");
+        expect(actual).to.be("fontFamily");
+    });
+
+    it("$.encodeCssString", function()
+    {
+        var css = {
+            fontFamily: "Arial, Helvetica",
+            color: "#232323",
+            borderLeftColor: "#fff",
+            borderWidth: "1px"
+        };
+
+        var actual = $.encodeCssString(css);
+
+        expect(actual).to.be("font-family:Arial, Helvetica;color:#232323;border-left-color:#fff;border-width:1px");
+    });
+
+    it("$.encodeCssString with trim", function()
+    {
+        var css = {
+            fontFamily: "  Arial, Helvetica  ",
+            color: " #232323  ",
+            borderLeftColor: " #fff ",
+            borderWidth: " 1px "
+        };
+
+        var actual = $.encodeCssString(css);
+
+        expect(actual).to.be("font-family:Arial, Helvetica;color:#232323;border-left-color:#fff;border-width:1px");
+    });
+
+    it("$.parseCssString", function()
+    {
+        var css = "font-family:Arial, Helvetica;color:#232323;border-left-color:#fff;border-width:1px";
+
+        var actual = $.parseCssString(css);
+
+        expect(actual.fontFamily).to.be("Arial, Helvetica");
+        expect(actual.color).to.be("#232323");
+        expect(actual.borderLeftColor).to.be("#fff");
+        expect(actual.borderWidth).to.be("1px");
+    });
+
+    it("$.parseCssString with trim", function()
+    {
+        var css = "font-family:Arial, Helvetica;color:#232323   ;border-left-color:#fff  ;border-width:1px ";
+
+        var actual = $.parseCssString(css);
+
+        expect(actual.fontFamily).to.be("Arial, Helvetica");
+        expect(actual.color).to.be("#232323");
+        expect(actual.borderLeftColor).to.be("#fff");
+        expect(actual.borderWidth).to.be("1px");
+    });
+});
---
+++ test/jquery.modalDialog.ajax.unittests.js
@@ -0,0 +1,64 @@
+﻿$.modalDialog.iframeLoadTimeout = 1000;
+$.modalDialog.animationDuration = 100;
+
+describe("jquery.modalDialog.ajax", function()
+{
+    it("Ensure ajax dialog content can be retrieved from a full HTML document", function(done)
+    {
+        var dialog = $.modalDialog.create({ url: "content/jquery.modalDialog.ajaxContent.fullHtml.html", ajax: true });
+
+        dialog
+            .open()
+            .then(
+                function()
+                {
+                    expect(dialog.getTitle()).to.be("ModalDialog ajax content, full HTML"); //, "Ensure title is extracted from the content's TITLE tag");
+
+                    expect($.trim(dialog.$container.find(".dialog-content").text())).to.be("Here's some ajax content");
+
+                    return dialog.close();
+                })
+            .then(done);
+    });
+
+    it("Ensure ajax dialog content can be retrieved from a partial HTML document", function(done)
+    {
+        var dialog = $.modalDialog.create({ url: "content/jquery.modalDialog.ajaxContent.html", ajax: true });
+
+        dialog
+            .open()
+            .then(
+                function()
+                {
+                    expect($.trim(dialog.$container.find(".dialog-content").text())).to.be("Here's some ajax content");
+
+                    return dialog.close();
+                })
+            .then(done);
+    });
+
+    it("Ensure ajax dialog title is derived from settings if specified", function(done)
+    {
+        var dialog = $.modalDialog.create({ 
+            url: "content/jquery.modalDialog.ajaxContent.fullHtml.html", 
+            ajax: true, 
+            title: "Title from settings"
+        });
+
+        dialog
+            .open()
+            .then(
+                function()
+                {
+                    expect(dialog.getTitle()).to.be("Title from settings");
+
+                    return dialog.close();
+                })
+            .then(done);
+    });
+});
+
+// window.onerror = function(msg)
+// {
+//     window.console.log("Uncaught error: " + msg);
+// };
