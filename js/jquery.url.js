24e22700c0c16292603e943e2c6e53bbf3d3a878
Laban Eilers <laban@eilers.com>
2013-10-25 10:44:29 -0400

    Diagnosing strange TravisCI failre

--- js/jquery.url.js
+++ js/jquery.url.js
@@ -240,6 +240,10 @@ $.Url = function(url)
             if (nextPartPos == -1)
             {
                 var cutOff = temp.substr(1); //cut off the initial ?
+                if (typeof cutOff == "undefined")
+                {
+                    throw new Error("cuttOff is undefined");
+                }
                 me.queryString = $.deparam(cutOff); 
                 temp = "";
             }
