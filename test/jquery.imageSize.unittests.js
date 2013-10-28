6ebb83cae8c57d0110eddf9b56c8d576e4f2b315
Laban Eilers <laban@eilers.com>
2013-10-28 07:32:16 -0400

    Unit tests for fitToBoundingBox()

--- test/jquery.imageSize.unittests.js
+++ test/jquery.imageSize.unittests.js
@@ -56,4 +56,64 @@ describe("jquery.imageSize plugin", function()
         });
     });
 
+    describe("jquery.rectWithAspectRatio()", function()
+    {
+        it("should fit a rect with a low aspect ratio into its container", function()
+        {
+            var rect = $.rectWithAspectRatio({ top: 0, left: 0, width: 100, height: 100}, 0.5);
+
+            assert.equal(rect.top, 0);
+            assert.equal(rect.left, 25);
+            assert.equal(rect.width, 50);
+            assert.equal(rect.height, 100);
+        });
+
+        it("should fit a rect with a high aspect ratio into its container", function()
+        {
+            var rect = $.rectWithAspectRatio({ top: 0, left: 0, width: 100, height: 100}, 2);
+
+            assert.equal(rect.top, 25);
+            assert.equal(rect.left, 0);
+            assert.equal(rect.width, 100);
+            assert.equal(rect.height, 50);
+        });
+
+        it("should fit a rect with an aspect ratio of 1 completely into its container", function()
+        {
+            var rect = $.rectWithAspectRatio({ top: 0, left: 0, width: 100, height: 100}, 1);
+
+            assert.equal(rect.top, 0);
+            assert.equal(rect.left, 0);
+            assert.equal(rect.width, 100);
+            assert.equal(rect.height, 100);
+        });
+    });
+
+    describe("jquery.fitToBoundingBox()", function()
+    {
+        it("should set an element's rect to match the specified image fit to the specified bounding box", function(done)
+        {
+            var $div = $("<img />").css("position", "absolute");
+
+            var callback = function()
+            {
+                $div.appendTo("body");
+
+                assert.equal($div.offset().top, 0);
+                assert.equal($div.offset().left, 25.5);
+                assert.equal($div.width(), 49); assert.equal($div.height(),
+                50);
+
+                $div.remove();
+
+                done();
+            };
+
+            $div.fitToBoundingBox("../images/clock.gif", { width: 100, height: 50 }, callback);
+
+            
+        });
+    });
+
+
 });
