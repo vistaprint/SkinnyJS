9f657fdb9eceda9ef5280b3dccb52bc09bc7194d
Adam Clayton <aclayton@vistaprint.com>
2014-02-11 12:11:19 -0500

    Renamed tutorialOverlayArrow -> tutorialOverlay.Arrow and tutorialOverlayTip -> tutorialOverlay.Tip

--- js/jquery.tutorialOverlay.header.js
+++ js/jquery.tutorialOverlay.header.js
@@ -2,5 +2,3 @@
 /// <reference path="jquery.proxyAll.js" />
 /// <reference path="jquery.rectUtils.js" />
 /// <reference path="jquery.calcRestrainedPos.js" />
-/// <reference path="jquery.tutorialOverlayTip.js" />
-/// <reference path="jquery.tutorialOverlayArrow.js" />
