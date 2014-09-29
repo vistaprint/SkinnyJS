69eb8d4850b29986bb94f64bb879d5a5e36438ea
sdevadas <sdevadas@vistaprint.com>
2014-09-29 10:35:38 -0400

    Replacing double quotes by single quotes for uniformity

--- js/jquery.richTooltip.js
+++ js/jquery.richTooltip.js
@@ -114,13 +114,13 @@
                 // Ignore z-index if position is set to a value where z-index is ignored by the browser
                 // This makes behavior of this function consistent across browsers
                 // WebKit always returns auto if the element is positioned
-                pos = elem.css( "position" );
-                if ( pos === "absolute" || pos === "relative" || pos === "fixed" ) {
+                pos = elem.css( 'position' );
+                if ( pos === 'absolute' || pos === 'relative' || pos === 'fixed' ) {
                     // IE returns 0 when zIndex is not specified
                     // other browsers return a string
                     // we ignore the case of nested elements with an explicit value of 0
                     // <div style="z-index: -10;"><div style="z-index: 0;"></div></div>
-                    zIndexVal = parseInt( elem.css( "zIndex" ), 10 );
+                    zIndexVal = parseInt( elem.css( 'zIndex' ), 10 );
                     if ( !isNaN( zIndexVal ) && zIndexVal !== 0 ) {
                         return zIndexVal;
                     }
